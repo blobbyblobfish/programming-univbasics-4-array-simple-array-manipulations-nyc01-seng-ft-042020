845797ae4909785af3343a1bb1987d150145e3b9
Alice Wang <aywang6@gmail.com>
2020-03-22 13:43:14 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift (array, string)
+  array.unshift(string)
+end
+
+def using_pop (array)
+  the_return = array.pop
+end
+
+def pop_with_args (array)
+  the_return = array.pop(2)
+end
+
+def using_shift (array)
+  the_return = array.pop(shift)
+end
+
+def shift_with_args ()
